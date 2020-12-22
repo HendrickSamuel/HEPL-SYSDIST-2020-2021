d345c18888e99526337e0caa991a157fd3e87618
Bene <benkama@hotmail.com>
2020-12-22 12:58:05 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Frontend/ShopSln/ShopWebApplication/Controllers/AuthentificationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopWebApplication.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/ShopSln/ShopWebApplication/Models/Authentification/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopWebApplication.Models.Authentification
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Nom d'utilisateur")]
+        public string Login { get; set; }
+        [Required]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+    }
+}
