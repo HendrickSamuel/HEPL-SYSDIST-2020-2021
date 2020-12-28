32488f5725daf3630365c3e9de8612d5e1f16bdf
Bene <benkama@hotmail.com>
2020-12-28 16:23:59 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Frontend/ShopSln/ShopWebApplication/Helpers/UserToken.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopWebApplication.Helpers
+{
+    public class UserToken
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        // public string Token { get; set; }
+
+
+        public UserToken()
+        {
+            Name = ""; Password = "";
+        }
+    }
+}
---
+++ Frontend/ShopSln/ShopWebApplication/Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using ShopWebApplication.Helpers;
+
+namespace ShopWebApplication.Services
+{
+    public interface ITokenService
+    {
+        public string GetToken(UserToken userToken);
+        protected bool InitToken();
+        public void SaveToken();
+    }
+}
