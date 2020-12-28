227033ae9483f366fb3d13e3abcd488a1a23b5f5
Samuel <samuel.hendrick@student.hepl.be>
2020-12-28 14:19:00 +0100

    Adding real users

--- general-service/src/main/java/io/hepl/generalservice/Security/MyUserDetailsService.java
+++ general-service/src/main/java/io/hepl/generalservice/Security/MyUserDetailsService.java
@@ -4,6 +4,8 @@
 
 package io.hepl.generalservice.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,12 +13,31 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private ArrayList<User> users;
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new User("sam","sam", new ArrayList<>());
+        for (User user: users) {
+            if(user.getUsername().equalsIgnoreCase(s))
+                return user;
+        }
+        return null;
+    }
+
+    @Bean
+    public ArrayList<User> getAllAuthorizedUsers()
+    {
+       return new ArrayList<User>(
+               Arrays.asList(
+                       new User("Asp-net","shop", new ArrayList<>()),
+                       new User("samuel","samuel", new ArrayList<>()),
+                       new User("benedict","benedict", new ArrayList<>())
+               ));
     }
 }
